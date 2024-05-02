7428bb556472ca04852647b16b274873f3f13810
Keerthi0810 <keerthanakeerthi30668@gmail.com>
2024-05-02 10:16:57 +0530

    .

---
+++ simple C++ program.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+   using namespace std;
+   void display()
+    {
+                   cout<<”C++ is better than C”;
+      }
+       int main()
+     {
+           display()
+           return 0;
+       }            
+
